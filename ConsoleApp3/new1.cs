cb2fbff3f043b828e6c74823954535c5d74a767f
Vbhaveen <bharadwajbhaveen@example.com>
2022-07-28 09:10:28 +0000

    commit

---
+++ ConsoleApp3/new1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public struct new1
+    {
+        public string name { get; set; }
+    }
+
+    public static class IntExtension
+    {
+        public static bool greaterthan(this int i, int value)
+            {
+            return i > value;
+          }
+
+        public static string reversing(this string str1)
+        {
+            int n = str1.Length;
+            string str2 = "";
+            for(int i =n-1; i >= 0; i--)
+            {
+                str2 += str1[i];
+            }
+            return str2;
+        }
+    }
+
+   
+}
